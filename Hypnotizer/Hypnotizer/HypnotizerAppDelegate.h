d9b598b81a242b68b62272d9b51b370bafebc28d
Tammer Saleh <tsaleh@gmail.com>
2011-12-22 17:54:42 -0800

    Page 125

--- Hypnotizer/Hypnotizer/HypnotizerAppDelegate.h
+++ Hypnotizer/Hypnotizer/HypnotizerAppDelegate.h
@@ -10,7 +10,7 @@
 
 @class HypnosisView;
 
-@interface HypnotizerAppDelegate : UIResponder <UIApplicationDelegate>
+@interface HypnotizerAppDelegate : UIResponder <UIApplicationDelegate, UIScrollViewDelegate>
 {
     HypnosisView *view;
 }
--- Hypnotizer/Hypnotizer/HypnotizerAppDelegate.m
+++ Hypnotizer/Hypnotizer/HypnotizerAppDelegate.m
@@ -16,11 +16,39 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     CGRect wholeWindow = [[self window] bounds];
-    view = [[HypnosisView alloc] initWithFrame:wholeWindow];
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:wholeWindow];
+    [[self window] addSubview:scrollView];
+    
+    CGRect reallyBigRect;
+    reallyBigRect.origin = CGPointZero;
+    reallyBigRect.size.width = wholeWindow.size.width * 2.0;
+    reallyBigRect.size.height = wholeWindow.size.height * 2.0;
+    [scrollView setContentSize:reallyBigRect.size];
+    
+    CGPoint offset;
+    offset.x = wholeWindow.size.width * 0.5;
+    offset.y = wholeWindow.size.height * 0.5;
+    [scrollView setContentOffset:offset];
+    
+    [scrollView setMinimumZoomScale:0.5];
+    [scrollView setMaximumZoomScale:5];
+    [scrollView setDelegate:self];
+    
+    view = [[HypnosisView alloc] initWithFrame:reallyBigRect];
     [view setBackgroundColor:[UIColor clearColor]];
-    [[self window] addSubview:view];
+    [scrollView addSubview:view];
+    [[self window] addSubview:scrollView];
     [[self window] makeKeyAndVisible];
+    
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
+    
     return YES;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return view;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
