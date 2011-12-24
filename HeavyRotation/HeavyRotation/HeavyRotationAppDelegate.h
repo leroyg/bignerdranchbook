835b5811558c71fd5d2e5cbba56ee05d15bb8a85
Tammer Saleh <tsaleh@gmail.com>
2011-12-24 11:11:50 -0800

    Page 159

---
+++ HeavyRotation/HeavyRotation/HeavyRotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HeavyRotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Tammer Saleh on 12/24/11.
+//  Copyright (c) 2011 Thunderbolt Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeavyRotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
