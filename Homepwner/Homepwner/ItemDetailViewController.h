b7eecec168b55e33b2ad29e30df29d7ba3683df0
Tammer Saleh <tsaleh@gmail.com>
2011-12-27 13:32:00 -0800

    Page 212

---
+++ Homepwner/Homepwner/ItemDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemDetailViewController.h
+//  Homepwner
+//
+//  Created by Tammer Saleh on 12/24/11.
+//  Copyright (c) 2011 Thunderbolt Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/Homepwner/ItemDetailViewController.m
@@ -0,0 +1,14 @@
+//
+//  ItemDetailViewController.m
+//  Homepwner
+//
+//  Created by Tammer Saleh on 12/24/11.
+//  Copyright (c) 2011 Thunderbolt Labs. All rights reserved.
+//
+
+#import "ItemDetailViewController.h"
+
+@implementation ItemDetailViewController
+
+
+@end
