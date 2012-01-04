c4348df4db3f55a6d2a48177dbc9bedeb036b7ef
Tammer Saleh <tsaleh@gmail.com>
2012-01-04 06:31:23 -0800

    Page 233

--- Homepwner/Homepwner/ImageStore.m
+++ Homepwner/Homepwner/ImageStore.m
@@ -8,6 +8,43 @@
 
 #import "ImageStore.h"
 
+static ImageStore *defaultImageStore = nil;
+
 @implementation ImageStore
+
++(id)allocWithZone:(NSZone *)zone {
+    return [self defaultImageStore];
+}
+
++(ImageStore *)defaultImageStore {
+    if (!defaultImageStore) {
+        defaultImageStore = [[super allocWithZone:NULL] init];
+    }
+    return defaultImageStore;
+}
+
+-(id)init {
+    if (defaultImageStore) {
+        return defaultImageStore;
+    }
+    
+    self = [super init];
+    if (self) {
+        dictionary = [[NSMutableDictionary alloc] init];
+    }
+}
+
+- (void)setImage:(UIImage *)i forKey:(NSString *)s {
+    [dictionary setObject:i forKey:s];
+}
+
+- (UIImage *)imageForKey:(NSString *)s {
+    return [dictionary objectForKey:s];
+}
+
+- (void)deleteImageForKey:(NSString *)s {
+    if (!s) { return; }
+    [dictionary removeObjectForKey:s];
+}
 
 @end
