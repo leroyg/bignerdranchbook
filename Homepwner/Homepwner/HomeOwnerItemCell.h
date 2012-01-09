e7905c8eec2220399ab43ca6c6aee3c970c207a9
Tammer Saleh <tsaleh@gmail.com>
2012-01-09 14:44:46 -0800

    Page 281

---
+++ Homepwner/Homepwner/HomeOwnerItemCell.h
@@ -0,0 +1,14 @@
+//
+//  HomeOwnerItemCell.h
+//  Homepwner
+//
+//  Created by Tammer Saleh on 1/9/12.
+//  Copyright (c) 2012 Thunderbolt Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface HomeOwnerItemCell : UITableViewCell
+
+@end
---
+++ Homepwner/Homepwner/HomeOwnerItemCell.m
@@ -0,0 +1,13 @@
+//
+//  HomeOwnerItemCell.m
+//  Homepwner
+//
+//  Created by Tammer Saleh on 1/9/12.
+//  Copyright (c) 2012 Thunderbolt Labs. All rights reserved.
+//
+
+#import "HomeOwnerItemCell.h"
+
+@implementation HomeOwnerItemCell
+
+@end
