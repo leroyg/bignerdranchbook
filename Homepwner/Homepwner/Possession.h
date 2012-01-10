ea83aee32b8ea0c85070fe8e6c882a6a0bd87aea
Tammer Saleh <tsaleh@gmail.com>
2012-01-10 10:16:36 -0800

    Page 299

--- Homepwner/Homepwner/Possession.h
+++ Homepwner/Homepwner/Possession.h
@@ -1,29 +1,29 @@
 //
 //  Possession.h
-//  RandomPossessions
+//  Homepwner
 //
-//  Created by Tammer Saleh on 12/16/11.
-//  Copyright (c) 2011 Thunderbolt Labs. All rights reserved.
+//  Created by Tammer Saleh on 1/10/12.
+//  Copyright (c) 2012 Thunderbolt Labs. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 
-@interface Possession : NSObject <NSCoding>
+@class AssetType;
 
-+ (id)randomPossession;
+@interface Possession : NSManagedObject
+
+@property (nonatomic, retain) NSDate * dateCreated;
+@property (nonatomic, retain) NSString * imageKey;
+@property (nonatomic, retain) NSNumber * orderingValue;
+@property (nonatomic, retain) NSString * possessionName;
+@property (nonatomic, retain) NSString * serialNumber;
+@property (nonatomic, retain) UIImage *thumbnail;
+@property (nonatomic, retain) NSData * thumbnailData;
+@property (nonatomic, retain) NSNumber * valueInDollars;
+@property (nonatomic, retain) AssetType *assetType;
+
 + (CGSize)thumbnailSize;
-
-@property(nonatomic, strong  ) NSString *possessionName;
-@property(nonatomic, strong  ) NSString *serialNumber;
-@property(nonatomic          ) int       valueInDollars;
-@property(nonatomic, strong) NSDate   *dateCreated;
-@property(nonatomic, strong  ) NSString *imageKey;
-@property(nonatomic, strong) UIImage  *thumbnail;
-@property(nonatomic, strong  ) NSData   *thumbnailData;
-
-- (id)initWithPossessionName:(NSString *)name
-             valueInDollars:(int)value
-               serialNumber:(NSString *)sNumber;
 
 - (void)setThumbnailDataFromImage:(UIImage *)image;
 
--- Homepwner/Homepwner/Possession.m
+++ Homepwner/Homepwner/Possession.m
@@ -1,97 +1,29 @@
 //
 //  Possession.m
-//  RandomPossessions
+//  Homepwner
 //
-//  Created by Tammer Saleh on 12/16/11.
-//  Copyright (c) 2011 Thunderbolt Labs. All rights reserved.
+//  Created by Tammer Saleh on 1/10/12.
+//  Copyright (c) 2012 Thunderbolt Labs. All rights reserved.
 //
 
 #import "Possession.h"
+#import "AssetType.h"
+
 
 @implementation Possession
 
-@synthesize possessionName, serialNumber, valueInDollars, dateCreated, imageKey, thumbnail, thumbnailData;
+@dynamic dateCreated;
+@dynamic imageKey;
+@dynamic orderingValue;
+@dynamic possessionName;
+@dynamic serialNumber;
+@dynamic thumbnail;
+@dynamic thumbnailData;
+@dynamic valueInDollars;
+@dynamic assetType;
 
 + (CGSize)thumbnailSize {
     return CGSizeMake(40, 40);
-}
-
-+ (id)randomPossession {
-    NSArray *randomAdjectiveList = [NSArray arrayWithObjects:@"Red", @"Blue", @"Purple", nil];
-    NSArray *randomNounList = [NSArray arrayWithObjects:@"car", @"truck", @"boat", nil];
-    int adjectiveIndex = rand() % [randomAdjectiveList count];
-    int nounIndex = rand() % [randomNounList count];
-    NSString *randomName = [NSString stringWithFormat:@"%@ %@",
-                            [randomAdjectiveList objectAtIndex:adjectiveIndex],
-                            [randomNounList objectAtIndex:nounIndex]];
-    int randomValue = rand() % 100;
-    NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
-                                    '0' + rand() % 10,
-                                    'A' + rand() % 26,
-                                    '0' + rand() % 10,
-                                    'A' + rand() % 26,
-                                    '0' + rand() % 10];
-    Possession *newPossession = [[self alloc] 
-                               initWithPossessionName:randomName 
-                               valueInDollars:randomValue 
-                               serialNumber:randomSerialNumber];
-    return newPossession;
-                                    
-}
-
-- (id)initWithCoder:(NSCoder *)decoder {
-    self = [super init];
-    if (self) {
-        [self setPossessionName:[decoder decodeObjectForKey:@"possessionName"]];
-        [self setSerialNumber:[decoder decodeObjectForKey:@"serialNumber"]];
-        [self setImageKey:[decoder decodeObjectForKey:@"imageKey"]];
-        [self setValueInDollars:[decoder decodeIntForKey:@"valueInDollars"]];
-        [self setThumbnailData:[decoder decodeObjectForKey:@"thumbnailData"]];
-        dateCreated = [decoder decodeObjectForKey:@"dateCreated"];
-    }
-    return self;
-}
-     
-
-- (id)initWithPossessionName:(NSString *)name valueInDollars:(int)value serialNumber:(NSString *)sNumber {
-    self = [super init];
-    if (self) {
-        [self setPossessionName:name];
-        [self setValueInDollars:value];
-        [self setSerialNumber:sNumber];
-        dateCreated = [[NSDate alloc] init];
-    }
-    return self;        
-}
-
-- (id)init {
-    return [self initWithPossessionName:@"Unknown" 
-                         valueInDollars:0 
-                           serialNumber:@"Unknown"];
-}
-
-- (UIImage *)thumbnail {
-    if (![self thumbnailData]) {
-        return nil;
-    }
-    
-    if (!thumbnail) {
-        thumbnail = [UIImage imageWithData:thumbnailData];
-    }
-    return thumbnail;
-}
-
-- (NSString *)description {
-    return [NSString stringWithFormat:@"%@ (%@) $%d [%@]", possessionName, serialNumber, valueInDollars, dateCreated];
-}
-
-- (void)encodeWithCoder:(NSCoder *)aCoder {
-    [aCoder encodeObject:possessionName forKey:@"possessionName"];
-    [aCoder encodeObject:serialNumber forKey:@"serialNumber"];
-    [aCoder encodeObject:dateCreated forKey:@"dateCreated"];
-    [aCoder encodeObject:imageKey forKey:@"imageKey"];
-    [aCoder encodeInt:valueInDollars forKey:@"valueInDollars"];
-    [aCoder encodeObject:thumbnailData forKey:@"thumbnailData"];
 }
 
 - (void)setThumbnailDataFromImage:(UIImage *)image {
@@ -121,5 +53,5 @@
     
     UIGraphicsEndImageContext();
 }
-                      
+
 @end
